2684eb4a58365b6a2340e07292bb4edf98f5107e
Hrihorii Ilin <devopsick@pm.me>
2023-09-23 23:25:43 +0000

    +

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import node from "@astrojs/node";
+import astroI18next from "astro-i18next";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [mdx(), sitemap(), react(), tailwind(), astroI18next()],
   output: "server",
   adapter: node({
     mode: "standalone"
